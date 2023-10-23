e5cf5a171efb50ff73459ced4bb9b1bb10b4eaea
Katie Amaral <kathryn_amaral@harvard.edu>
2023-10-23 18:19:03 -0400

    Update env example

--- main.py
+++ main.py
@@ -4,14 +4,17 @@
 import os
 from langchain.llms import OpenAI
 from langchain.chat_models import ChatOpenAI
+from langchain.schema import HumanMessage
 
 openai_api_key = os.environ.get("OPENAI_API_KEY")
+
+primo_api_key = os.environ.get("PRIMO_API_KEY")
 
 llm = OpenAI()
 #chat_model = ChatOpenAI()
 
-llm_result = llm.predict("hieeeeeee!")
-print(llm_result)
+#llm_result = llm.predict("hieeeeeee!")
+#print(llm_result)
 
 #chat_result = chat_model.predict("hi!")
 #print(chat_result)
@@ -21,3 +24,12 @@ print(llm_result)
 #llm.predict(text)
 
 #chat_model.predict(text)
+
+text = "What would be a good company name for a company that makes colorful socks?"
+messages = [HumanMessage(content=text)]
+
+llm.predict_messages(messages)
+# >> Feetful of Fun
+
+chat_model.predict_messages(messages)
+# >> Socks O'Color
