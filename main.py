efb2003fda8e1a74102d9edac55c5f1a56839a41
Katie Amaral <kathryn_amaral@harvard.edu>
2023-10-23 21:56:17 -0400

    Prompt template

--- main.py
+++ main.py
@@ -5,31 +5,28 @@ import os
 from langchain.llms import OpenAI
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import HumanMessage
+from langchain.schema.messages import SystemMessage
+from langchain.schema import BaseOutputParser
+from langchain.prompts.chat import ChatPromptTemplate
+from langchain.prompts import HumanMessagePromptTemplate
+chat_model = ChatOpenAI()
 
 openai_api_key = os.environ.get("OPENAI_API_KEY")
-
 primo_api_key = os.environ.get("PRIMO_API_KEY")
 
-llm = OpenAI()
-#chat_model = ChatOpenAI()
+system_content = """You are a friendly assistant who helps to find information about books.
+A user will pass in a book subject, and you should generate a list of books related to that subject.
+ONLY return information related to the books they ask about, and nothing more."""
+human_template = "I'm looking for books on this subject: {subject}"
 
-#llm_result = llm.predict("hieeeeeee!")
-#print(llm_result)
+chat_template = ChatPromptTemplate.from_messages(
+    [
+        SystemMessage(
+            content=(system_content)
+        ),
+        HumanMessagePromptTemplate.from_template(human_template),
+    ]
+)
 
-#chat_result = chat_model.predict("hi!")
-#print(chat_result)
-
-#text = "What would be a good company name for a company that makes colorful socks?"
-
-#llm.predict(text)
-
-#chat_model.predict(text)
-
-text = "What would be a good company name for a company that makes colorful socks?"
-messages = [HumanMessage(content=text)]
-
-llm.predict_messages(messages)
-# >> Feetful of Fun
-
-chat_model.predict_messages(messages)
-# >> Socks O'Color
+chat_result = chat_model(chat_template.format_messages(subject='dogs'))
+print(chat_result)
