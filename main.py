26d8be0c8caa449e18c4231bc0f669fd9bb98481
Katie Amaral <kathryn_amaral@harvard.edu>
2023-10-26 17:27:36 -0400

    Add documentation for Primo API and libraries

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # pipenv shell
 # pipenv run python3 main.py
-import os, requests, json
+import asyncio, os, requests, json, csv
 from langchain.llms import OpenAI
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import APIChain
@@ -17,8 +17,6 @@ from langchain.agents.agent_toolkits import JsonToolkit
 from langchain.chains import LLMChain
 from langchain.requests import TextRequestsWrapper
 from langchain.tools.json.tool import JsonSpec
-import asyncio
-import csv
 
 llm = OpenAI(temperature=0)
 chat_model = ChatOpenAI()
@@ -26,9 +24,9 @@ openai_api_key = os.environ.get("OPENAI_API_KEY")
 primo_api_key = os.environ.get("PRIMO_API_KEY")
 primo_api_host = os.environ.get("PRIMO_API_HOST")
 
-async def open_csv_file():
+async def open_csv_file(path):
     rows = []
-    with open('libraries.csv', 'r') as file:
+    with open(path, 'r') as file:
         reader = csv.reader(file)
         for row in reader:
             # Process each row asynchronously
@@ -36,57 +34,31 @@ async def open_csv_file():
             rows.append(processed_row)
     return rows
 
+async def open_text_file(path):
+    with open(path, 'r') as f:
+        return f.read()
+
+async def open_json_file(path):
+    with open(path) as json_file:
+        json_data = json.load(json_file)
+        print(json_data)
+        return json_data
+
 async def process_row(row):
     # Perform some processing on each row asynchronously
     return row
 
 async def main():
-    libraries_csv = await open_csv_file()
-    print(libraries_csv)
+    libraries_csv = await open_csv_file('libraries.csv')
+    primo_api_docs = await open_text_file('primo_api_docs.txt')
+    primo_api_schema = await open_json_file('primo_api_schema.json')
+    primo_api_docs = primo_api_docs.replace('primo_api_host', primo_api_host)
+    primo_api_docs = primo_api_docs.replace('primo_api_key', primo_api_key)
+    primo_api_docs = primo_api_docs.replace('libraries_csv', '{}'.format(libraries_csv))
+    print(primo_api_docs)
 
     # https://developers.exlibrisgroup.com/primo/apis/search/
     # https://developers.exlibrisgroup.com/wp-content/uploads/primo/openapi/primoSearch.json
-    primo_api_docs = f"""
-      (Instructions for generating parameters dynamically are indicated in parantheses.)\n\n
-      HTML documentation: https://developers.exlibrisgroup.com/primo/apis/docs/primoSearch/R0VUIC9wcmltby92MS9zZWFyY2g=/#queryParameters
-      JSON schema: https://developers.exlibrisgroup.com/wp-content/uploads/primo/openapi/primoSearch.json
-      BASE URL: {primo_api_host}\n\n
-      Request: GET /primo/v1/search\n\n
-      Required hard-coded parameters:\n\n
-          (these required parameters must be hard-coded to these exact values)\n\n
-          facet=rtype,include,books\n\n
-          scope=default_scope\n\n
-          tab=books\n\n
-          vid=HVD2\n\n
-          limit=1\n\n
-          offset=0\n\n
-          apikey={primo_api_key}\n\n
-      Required dynamic search query parameters:\n\n
-          (these required parameters are must be generated dynamically based on human input question)\n\n
-          (q should be generated based on human input question e.g. any,contains,dogs as companions)\n\n
-          q=<field_1>,<precision_1>,<value_1>[[,<operator_1>];<field_n>,<precision_n>,<value_n>...]
-              * field - The data field that you want to search within. The following fields are valid: any (for any field), title, creator (for author), sub (for subject), and usertag (for tag).
-              * precision - The precision operation that you want to apply to the field. The following precision operators are valid: exact (value must match the data in the field exactly), begins_with (the value must be found at the beginning of the field), and contains (the value must be found anywhere in the field).
-              * value - The search terms, which can be a word, phrase, or exact phrase (group of words enclosed by quotes), and can include the following logical operators: AND, OR, and NOT. For more information regarding search terms, see Performing Basic Searches.
-              * operator - When specifying multiple search fields for advanced searches, this parameter applies the following logical operations between fields: AND (specified values must be found in both fields), OR (specified values must be found in at least one of the fields), NOT (the specified value of the next field must not be found). If no operator is specified, the system defaults to AND.
-
-              Note: Multiple fields are delimited by a semicolon.
-              Limitation: The value must not include a semicolon character.
-
-              In the following example, the system searches for all records in which the word home is found anywhere within the record's title:
-              q=title,contains,home
-
-              In the following example, the system searches for all records in which the title field contains the words pop and music and the subject field contains the word korean:
-              q=title,contains,pop music,AND;sub,contains,korean
-          (language ISO 639-1 language code e.g. 'en' should be automatically deteted based on human input question)\n\n
-          lang=<language>\n\n
-          (facet 'tlevel' should be generated based on human input question. please always set the 'tlevel' facet. the default 'tlevel' value should be 'facet=tlevel,include,available' to return books that are available onsite OR in storage. if they want books that are available on-site only, use this facet query: 'facet=tlevel,include,available_onsite' \n\n
-          facet=tlevel,include,<available or available_onsite>
-          (facet 'library' should be generated using Library Codes from the "libraries_csv" csv file. the "libraries_csv" csv file is in between '>>>' and '<<<' at the bottom of this documentation. please create a new facet for each library code, e.g. 'facet=library,include,<Library Code 1>&facet=library,include,<Library Code 2>&&facet=library,include,<Library Code n>'. if the user does not mention any specific libraries, please include all library codes in the "libraries_csv" csv file. if the user mentions that they want results from certain libraries, please create an individual facet for EACH library that is mentioned in the question. please use both the "Display name in Primo API" and "How users may refer to it" in the "libraries_csv" csv file to determine what library codes to use based on the human input question. for example, if they ask "I want books in Lamont, Baker, or Kennedy" there should be three facets, one for Lamont, one for Baker, and one for Kennedy e.g. facet=library,include,LAM&facet=library,include,KSG&facet=library,include,BAK\n\n
-          facet=library,include,<a Library Code from the "libraries_csv" csv file>
-          >>>{libraries_csv}<<<
-      """
-    print(primo_api_docs)
     """ Step 1: Generate API request to HOLLIS based on human input question """
     headers = {"Content-Type": "application/json"}
     # https://github.com/langchain-ai/langchain/blob/3d74d5e24dd62bb3878fe34de5f9eefa6d1d26c7/libs/langchain/langchain/chains/api/prompt.py#L4
@@ -100,7 +72,7 @@ async def main():
     #api_result = chain.run("Generate a GET request to search the Primo API to find books about dogs.")
     #api_result = chain.invoke("Generate a GET request to search the Primo API to find books to answer the human's input question. {human_input_question}".format(human_input_question=human_input_question))
     primo_api_request = get_request_chain.run(question=human_input_question, api_docs=primo_api_docs)
-    print(primo_api_request)
+    print(primo_api_request.replace(primo_api_key, "PRIMO_API_KEY"))
 
     """ Step 2: Write logic to filter, reduce, and prioritize data from HOLLIS """
 
