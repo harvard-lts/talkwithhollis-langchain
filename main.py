2d715c815934ebe0eceff064627ffaa9c3d6edd8
Katie Amaral <kathryn_amaral@harvard.edu>
2023-11-01 23:00:38 -0400

    Refactor prompt template and use llm.predict to generate query

--- main.py
+++ main.py
@@ -16,8 +16,9 @@ from langchain.agents.agent_toolkits import JsonToolkit
 from langchain.chains import LLMChain
 from langchain.requests import TextRequestsWrapper
 from langchain.tools.json.tool import JsonSpec
+from langchain.prompts.prompt import PromptTemplate
 llm = OpenAI(temperature=0)
-chat_model = ChatOpenAI()
+chat_model = ChatOpenAI(temperature=0)
 openai_api_key = os.environ.get("OPENAI_API_KEY")
 primo_api_key = os.environ.get("PRIMO_API_KEY")
 primo_api_host = os.environ.get("PRIMO_API_HOST")
@@ -52,7 +53,7 @@ def generate_primo_api_request(llm_response):
     primo_api_request = primo_api_host + f"?scope=default_scope&tab=books&vid=HVD2&limit={primo_api_limit}&offset=0&apikey={primo_api_key}&q=any,contains,{'%20'.join(llm_response['keywords'])}&multiFacets=facet_rtype,include,books"
     if len(llm_response['libraries']) > 0:
         primo_api_request += "%7C,%7Cfacet_library,include," + '&facet_library,include,'.join(llm_response['libraries'])
-    primo_api_request += "&facet_tlevel,include,available_onsite"
+    primo_api_request += "%7C,%7Cfacet_tlevel,include,available_onsite"
     return primo_api_request
 
 def shrink_results_for_llm(results, libraries):
@@ -75,39 +76,53 @@ async def main(human_input_text):
 
     # https://developers.exlibrisgroup.com/primo/apis/search/
     # https://developers.exlibrisgroup.com/wp-content/uploads/primo/openapi/primoSearch.json
-    """ Step 1: Generate API request to HOLLIS based on human input question """
+    # Step 1: Generate API request to HOLLIS based on human input question
     headers = {"Content-Type": "application/json"}
 
-    user_input_system_content = """You will receive a user prompt in which a user will be searching for books with certain qualities.
-    From that prompt, extract the keywords that describe the books, do not create keywords related to how the user intender to use the books.
-    Create a list of the three-letter Library Codes of the libraries the user requested.\n
-    Return a json object containing lists of keywords and the Library Codes of requested libraries.\n"""
-    user_input_human_query_string = human_input_text
-    parse_user_input_chat_template = ChatPromptTemplate.from_messages(
-        [
-            SystemMessage(
-                content=(user_input_system_content)
-            ),
-            HumanMessagePromptTemplate.from_template(user_input_human_query_string),
-        ]
-    )
-    user_input_chain = parse_user_input_chat_template | chat_model
-    prompt_chat_result = user_input_chain.invoke({"human_input_text": "Context:\n[CONTEXT]\n" + str(libraries_csv) + "\n[/CONTEXT]\n\n " + user_input_human_query_string})
-    prompt_result = json.loads(prompt_chat_result.content)
-    print(prompt_result)
+    # https://github.com/langchain-ai/langchain/blob/3d74d5e24dd62bb3878fe34de5f9eefa6d1d26c7/libs/langchain/langchain/chains/api/prompt.py#L4
+    querystring_template = """You are given a user question asking to find books by keyword.
+    The user also may mention that they want books from certain libraries.
+    From the user question, extract a list of keywords that describe the books e.g. ['cybercrime', 'malware', 'DDoS'].
+    Exclude keywords related to how the user intends to use the books e.g. 'research' or 'study'.
+    Exclude any keywords that could be considered harmful, offensive, or inappropriate.
+    From the user question, also generate a list of three-letter Library Codes from the Libraries CSV file based on the user question.
+    If the user does not mention any specific libraries in the question, generate a list of all Library Codes.
+    If the user mentions that they want results from certain libraries, generate a list of ONLY the Library Codes mentioned, using ONLY the exact value of the Library Code.
+    Use both the "Display name in Primo API" and "How users may refer to it" columns to determine what Library Codes to use based on the user question.
+    User Question:{user_question}
+    Libraries CSV file:{libraries_csv}
+    Return a single json object only. The object must contain two properties, 'keywords' with a list of keywords and 'libraries' with list of the Library Codes for the requested libraries.
+    """
 
-    primo_api_request = generate_primo_api_request(prompt_result)
+    qs_prompt_template = PromptTemplate.from_template(template=querystring_template)
+
+    # format the prompt to add variable values
+    qs_prompt_formatted_str: str = qs_prompt_template.format(
+      user_question=human_input_text, libraries_csv=libraries_csv
+    )
+    print(qs_prompt_formatted_str)
+
+    # make a prediction
+    qs_prediction = llm.predict(qs_prompt_formatted_str)
+
+    # print the prediction
+    print("qs_prediction")
+    print(qs_prediction)
+    qs_prompt_result = json.loads(qs_prediction)
+    print("qs_prompt_result")
+    print(qs_prompt_result)
+
+    primo_api_request = generate_primo_api_request(qs_prompt_result)
     print(primo_api_request)
 
-    response = requests.get(primo_api_request)
+    primo_api_response = requests.get(primo_api_request)
 
-    """ Step 2: Write logic to filter, reduce, and prioritize data from HOLLIS using python methods and LLMs"""
-    reduced_results, included_libraries_set = shrink_results_for_llm(response.json()['docs'][0:max_results_to_llm], prompt_result['libraries'])
+    # Step 2: Write logic to filter, reduce, and prioritize data from HOLLIS using python methods and LLMs
+    reduced_results, included_libraries_set = shrink_results_for_llm(primo_api_response.json()['docs'][0:max_results_to_llm], qs_prompt_result['libraries'])
 
     print(included_libraries_set)
     
-    """ Step 3: Context injection into the chat prompt """
-
+    # Step 3: Context injection into the chat prompt
     system_content = """You are a friendly assistant who helps to find information about the locations and availability of books in a network of libraries.
     # Return a list books with their titles, authors, and call numbers.
     # The list should be divided into categories by location, and each location should have the header of the location name + 9am - 5pm"""
