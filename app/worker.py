b0a2f402b669abaa2171945bdad2e544ed1cd219
Katie Amaral <kathryn_amaral@harvard.edu>
2023-11-15 21:03:48 -0500

    Prompt template works

--- app/worker.py
+++ app/worker.py
@@ -21,7 +21,12 @@ max_results_to_llm = int(os.environ.get("MAX_RESULTS_TO_LLM", 5))
 
 ai_platform = os.environ.get("AI_PLATFORM", "azure")
 
-sample_json = {
+example_query_result_json = {
+    "keywords": ["cybercrime", "malware", "DDoS"],
+    "libraries": ["BAK", "SEC", "WID"]
+}
+
+example_chat_result_json = {
     "LAM": [
         {
             'title': ['The Art of Computer Programming'],
@@ -52,13 +57,13 @@ class LLMWorker():
             os.environ["OPENAI_API_VERSION"] = os.environ.get("AZURE_OPENAI_API_VERSION", "2023-08-01-preview")
             os.environ["OPENAI_API_BASE"] = os.environ.get("AZURE_OPENAI_API_BASE")
             os.environ["OPENAI_API_KEY"] = os.environ.get("AZURE_OPENAI_API_KEY")
-
+            """
             self.chat_model = AzureChatOpenAI(
                 temperature=0,
                 deployment_name=os.environ.get("AZURE_OPENAI_API_DEPLOYMENT", "gpt-35-turbo"),
                 model_version=os.environ.get("AZURE_OPENAI_API_MODEL_VERSION", "0301"),
             )
-            """
+
             self.llm = AzureOpenAI(
                 deployment_name=os.environ.get("AZURE_OPENAI_API_DEPLOYMENT", "gpt-35-turbo"),
                 model_name=os.environ.get("AZURE_OPENAI_API_MODEL_NAME", "gpt-35-turbo"),
@@ -134,26 +139,34 @@ class LLMWorker():
         headers = {"Content-Type": "application/json"}
 
         # https://github.com/langchain-ai/langchain/blob/3d74d5e24dd62bb3878fe34de5f9eefa6d1d26c7/libs/langchain/langchain/chains/api/prompt.py#L4
-        querystring_template = """You are given a user question asking to find books by keyword.
-        The user also may mention that they want books from certain libraries.
-        From the user question, extract a list of keywords that describe the books e.g. ['cybercrime', 'malware', 'DDoS'].
-        If you cannot find any keywords, the keywords list should be empty.
-        Exclude keywords related to how the user intends to use the books e.g. 'research' or 'study'.
-        Exclude any keywords that could be considered harmful, offensive, or inappropriate.
-        From the user question, also generate a list of three-letter Library Codes from the Libraries CSV file based on the user question.
-        If the user does not mention any specific libraries in the question, generate a list of all Library Codes.
-        If the user mentions that they want results from certain libraries, generate a list of ONLY the Library Codes mentioned, using ONLY the exact value of the Library Code.
-        Use both the "Display name in Primo API" and "How users may refer to it" columns to determine what Library Codes to use based on the user question.
-        User Question:{user_question}
-        Libraries CSV file:{libraries_csv}
-        Return a single json object only. The object must contain two properties, 'keywords' with a list of keywords and 'libraries' with list of the Library Codes for the requested libraries.
+        querystring_template = """You are a helpful AI assistant expert in identifying the relevant keywords and library codes based on the user's question about books currently available in libraries.\n
+        Use following context to create the keywords and library codes. Context:\n\n
+        You are given a user question asking to find books by keyword.\n
+        The user also may mention that they want books from certain libraries.\n
+        From the user question, extract a list of keywords that describe the books e.g. ['cybercrime', 'malware', 'DDoS'].\n
+        If you cannot find any keywords, the keywords list should be empty.\n
+        Exclude keywords related to how the user intends to use the books e.g. 'research' or 'study'.\n
+        Exclude any keywords that could be considered harmful, offensive, or inappropriate.\n
+        From the user question, also generate a list of three-letter Library Codes from the Libraries CSV file based on the user question.\n
+        If the user does not mention any specific libraries in the question, generate a list of all Library Codes.\n
+        If the user mentions that they want results from certain libraries, generate a list of ONLY the Library Codes mentioned, using ONLY the exact value of the Library Code.\n
+        Use both the "Display name in Primo API" and "How users may refer to it" columns to determine what Library Codes to use based on the user question.\n
+        User Question:\n{human_input_text}\n
+        Libraries CSV file:\n{libraries_csv}\n
+        Use the following format for the return value:\n\n
+        Return a valid json object only.\n
+        The json object must have two properties, 'keywords' and 'libraries' only.\n
+        The 'keywords' value must be a list of keywords and the 'libraries' value must be a list of the Library Codes for the requested libraries.
+        Example JSON result:\n{example_query_result_json}\n
         """
 
         qs_prompt_template = PromptTemplate.from_template(template=querystring_template)
 
         # format the prompt to add variable values
         qs_prompt_formatted_str: str = qs_prompt_template.format(
-        user_question=human_input_text, libraries_csv=libraries_csv
+          human_input_text=human_input_text,
+          libraries_csv=libraries_csv,
+          example_query_result_json=json.dumps(example_query_result_json)
         )
 
         # make a prediction
@@ -222,7 +235,7 @@ class LLMWorker():
             \n\n
             Below is an example input and output with example data:\n
             input json:\n
-            {json.dumps(sample_json)}
+            {json.dumps(example_chat_result_json)}
             
             output:
             Here are some books I found for you:\n
